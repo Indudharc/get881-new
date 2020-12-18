7984f4a7da7de3ced853e3e883e9619ee59f05de
Indudhar <Indudhar@DESKTOP-47OVU3P>
2020-12-18 16:41:58 +0530

    added subs in br1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,10 +5,8 @@ public class Calc {
 		System.out.println("Summation="+(num1+num2));
 	}
 	public void subs(int num1,int num2) {
-		System.out.println("Diff="+(num1-num2));
+		System.out.println("Difference="+(num1-num2));
 	}
-	public void prod(int num1,int num2) {
-		System.out.println("product="+(num1*num2));
-	}
+	
 
 }
